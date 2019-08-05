68baf9ed617c1fc64a3b35a9f51ee7a157388cbe
Justin Jameson <jjameson@uw.edu>
2019-08-04 18:53:38 -0700

    updating

--- mysite/blogging/views.py
+++ mysite/blogging/views.py
@@ -8,7 +8,17 @@ def list_view(request):
     published = Post.objects.exclude(published_date__exact=None)
     posts = published.order_by('-published_date')
     context = {'posts': posts}
-    return render(request, 'list.html', context)
+    return render(request, 'blogging/list.html', context)
+
+
+def detail_view(request, post_id):
+    published = Post.objects.exclude(published_date__exact=None)
+    try:
+        post = published.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404
+    context = {'post': post}
+    return render(request, 'blogging/detail.html', context)
 
 
 def stub_view(request, *args, **kwargs):
